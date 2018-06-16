6c88802090d8cebe5d8827ffa2c7f9a450537e84
Rajeev <belmakov@yahoo.com>
2018-06-16 07:58:45 +0530

    initial commit

---
+++ CommunityManager/CommunityPortal/Models/Unit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunityPortal.Models
+{
+    public class Unit
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public int Floor { get; set; }
+
+        public Guid TenantID { get; set; }
+        public Guid OwnerID { get; set; }
+    }
+}
